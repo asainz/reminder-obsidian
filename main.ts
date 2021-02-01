d6af7dfcb1b4920bea689d72d02515c114e52c13
Andres Sainz <andressainz@gmail.com>
2021-02-01 23:06:19 +0100

    remove comments

--- main.ts
+++ main.ts
@@ -37,8 +37,6 @@ export default class ReminderPlugin extends Plugin {
 	settings: ReminderPluginSettings;
 
 	async onload() {
-		console.log('loading plugin');
-
 		let naturalLanguageDates = this.app.plugins.getPlugin('nldates-obsidian');
 		if (!naturalLanguageDates) {
 			new Notice("The Natural Language Dates plugin was not found. The Reminder plugin requires the Natural Language Dates plugin. Please install it first and make sure it is enabled before using Review.");
@@ -46,29 +44,6 @@ export default class ReminderPlugin extends Plugin {
 
 		await this.loadSettings();
 
-		// this.addRibbonIcon('dice', 'Sample Plugin', () => {
-		// 	new Notice('This is a notice!');
-		// });
-
-		// this.addStatusBarItem().setText('Status Bar Text');
-
-		// this.addCommand({
-		// 	id: 'open-sample-modal',
-		// 	name: 'Open Sample Modal',
-		// 	// callback: () => {
-		// 	// 	console.log('Simple Callback');
-		// 	// },
-		// 	checkCallback: (checking: boolean) => {
-		// 		let leaf = this.app.workspace.activeLeaf;
-		// 		if (leaf) {
-		// 			if (!checking) {
-		// 				new SampleModal(this.app).open();
-		// 			}
-		// 			return true;
-		// 		}
-		// 		return false;
-		// 	}
-		// });
 		this.addCommand({
 			id: 'save-reminders',
 			name: 'Save reminders',
@@ -76,24 +51,6 @@ export default class ReminderPlugin extends Plugin {
 		});
 
 		this.addSettingTab(new ReminderPluginSettingsTab(this.app, this));
-
-		this.registerCodeMirror((cm: CodeMirror.Editor) => {
-			// console.log('codemirror', cm);
-		});
-
-		const app = this.app;
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			// console.log('click', app.metadataCache);
-			// console.log(app.workspace.activeLeaf.getDisplayText());
-			// console.log(app.workspace.activeLeaf.view.file);
-			const noteFile = app.workspace.activeLeaf.view.file;
-			app.vault.read(noteFile).then(function (result) {
-				// let previousNoteText = result;
-				// let newNoteText = previousNoteText.replace(someBlock, lineWithBlock);
-				// app.vault.modify(noteFile, newNoteText);
-				// console.log(result)
-			})
-		});
 
 		// this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
@@ -104,15 +61,6 @@ export default class ReminderPlugin extends Plugin {
 	}
 
 	async saveReminders() {
-		// TODO:
-		// Get reminders from current daily note
-		// For each reminder, get date and content
-		// for each reminder, check if the daily note for that day has been created
-		// if yes, append the reminder
-		// if no, check if theres a daily note template
-		// create daily note 
-		// append the reminder
-
 		const file = this.app.workspace.activeLeaf.view.file;
 		this.app.vault.read(file).then((content: string) => {
 			const lines = content.split('\n').map(l => l.startsWith('- ') ? l.replace('- ', '') : l);
@@ -120,21 +68,6 @@ export default class ReminderPlugin extends Plugin {
 
 			let items = {};
 			reminders.forEach(reminder => {
-				console.log(reminder);
-
-				// let notesFolder = this.settings.dailyNotesFolder;
-				// let notesPath = "";
-				// if (notesFolder === "") {
-				// 	notesPath = "/"; // If the user is using the root for their daily notes, don't add a second /.
-				// } else {
-				// 	notesPath = "/" + notesFolder + "/";
-				// }
-
-				// need to create something links this
-				// {
-				// 	'daily_notes/08.02.2021': []
-				// }
-
 				if (!items[reminder.moment.formattedString]) {
 					items[reminder.moment.formattedString] = [];
 				}
@@ -164,24 +97,6 @@ export default class ReminderPlugin extends Plugin {
 					this.app.vault.modify(dailyNoteFile, newDailyNoteContent);
 				});
 			});
-
-			// reminders.forEach((reminder: string) => {
-			// 	const content = reminder.split('@')[0];
-			// 	const when = reminder.split('@')[1] || this.settings.defaultDate;
-
-			// 	let naturalLanguageDates = this.app.plugins.getPlugin('nldates-obsidian');
-
-			// 	const whenDate = naturalLanguageDates.parseDate(when);
-			// 	// console.log(this.settings.dailyNotesFolder);
-
-			// 	let notesFolder = this.settings.dailyNotesFolder;
-			// 	let notesPath = "";
-			// 	if (notesFolder === "") {
-			// 		notesPath = "/"; // If the user is using the root for their daily notes, don't add a second /.
-			// 	} else {
-			// 		notesPath = "/" + notesFolder + "/";
-			// 	}
-			// });
 		});
 	}
 
@@ -197,22 +112,6 @@ export default class ReminderPlugin extends Plugin {
 		await this.saveData(this.settings);
 	}
 }
-
-// class SampleModal extends Modal {
-// 	constructor(app: App) {
-// 		super(app);
-// 	}
-
-// 	onOpen() {
-// 		let { contentEl } = this;
-// 		contentEl.setText('Woah!');
-// 	}
-
-// 	onClose() {
-// 		let { contentEl } = this;
-// 		contentEl.empty();
-// 	}
-// }
 
 class ReminderPluginSettingsTab extends PluginSettingTab {
 	plugin: ReminderPlugin;
